9baa982c25996acc6e5b7e33df0aa744f0e8f60c
Mohammed Abdul Mannan <abdulmannan34695@gmail.com>
2025-04-15 09:51:50 -0500

    Update oauth2callback.py

--- Handlers/Reservations/oauth2callback.py
+++ Handlers/Reservations/oauth2callback.py
@@ -3,15 +3,26 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from flask import session, render_template_string
 import json
+import os
 
 class OAuth2CallbackHandler:
     def __init__(self):
-        with open('config.json', 'r') as f:
-            config = json.load(f)
-            self.scopes = config['SCOPES']
         
-        self.credentials_path = 'credentials.json'
-        self.redirect_uri = 'http://localhost:5000/oauth2callback'
+        self.credentials_path = os.environ.get('CREDENTIALS_PATH', './credentials.json')
+        self.config_path = os.environ.get('CONFIG_PATH', './config.json')
+            
+        try:
+            with open(self.config_path, 'r') as f:
+                config = json.load(f)
+                self.scopes = config['SCOPES']
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Config file not found at {self.config_path}")
+        except json.JSONDecodeError:
+            raise ValueError(f"Invalid JSON in config file at {self.config_path}")
+        except KeyError:
+            raise KeyError("'SCOPES' key not found in config file")
+        
+        self.redirect_uri = os.environ.get('OAUTH2CALLBACK_URI', 'http://localhost:5000/oauth2callback')
 
     def handle_callback(self, code, state):
         try:
